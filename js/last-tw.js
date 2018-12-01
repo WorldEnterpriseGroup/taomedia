5fd1ed28121a76230fd8d13eae1f0576f3a03ad9
Matthew Hackney <mail@matthewhackney.com>
2018-11-30 19:15:53 -0500

    New Format

--- js/last-tw.js
+++ js/last-tw.js
@@ -1,41 +1,46 @@
-function twitterCallback2(twitters) {
-  var statusHTML = [];
-  for (var i=0; i<twitters.length; i++){
-    var username = twitters[i].user.screen_name;
-    var status = twitters[i].text.replace(/((https?|s?ftp|ssh)\:\/\/[^"\s\<\>]*[^.,;'">\:\s\<\>\)\]\!])/g, function(url) {
-      return '<a href="'+url+'">'+url+'</a>';
-    }).replace(/\B@([_a-z0-9]+)/ig, function(reply) {
-      return  '<a href="http://twitter.com/'+reply.substring(1)+'">'+ reply.charAt(0) + reply.substring(1)+'</a>';
-    }).replace(/\B#([_a-z0-9а-я]+)/ig, function(hashtag) {
-      return '<a href="http://twitter.com/search?q=%23'+hashtag.substring(1)+'">'+hashtag+'</a>';
-    });
-    statusHTML.push('<p class="twitter-author"><a href="http://twitter.com/'+username+'" target="_blank">@'+username+'</a></p><p class="tweet">' +status+ '</p><p class="twitter-date">'+relative_time(twitters[i].created_at)+'</p>');
-  }
-  document.getElementById('lasttweet').innerHTML = statusHTML.join('');
-  console.log(1);
+var buildString = "";
+ 
+$(document).ready(function() {
+	/*for(var i=0;i < tweetUsers.length;i++) {
+		if(i!=0) buildString+='+OR+';
+		buildString+='from:'+tweetUsers[i];
+	}*/
+	var fileref = document.createElement('script');	
+	fileref.setAttribute("type","text/javascript");
+	fileref.setAttribute("src", "http://search.twitter.com/search.json?q=&tag=twitter&lang=en&callback=TweetTick&rpp=1");
+	document.getElementsByTagName("head")[0].appendChild(fileref);
+});
+ 
+function TweetTick(ob) {
+	var container=$('#parallax2 .twtr');
+	container.html('');	
+	$(ob.results).each(function(el) {
+	var str = '<p class="twitter-author"><a href="http://twitter.com/'+this.from_user+'" target="_blank">@'+this.from_user+'</a></p><p class="tweet">' + formatTwitString(this.text) + '</p><p class="twitter-date">'+relativeTime(this.created_at)+'</p>';
+	container.append(str);	
+	});	
 }
-
-function relative_time(time_value) {
-  var values = time_value.split(" ");
-  time_value = values[1] + " " + values[2] + ", " + values[5] + " " + values[3];
-  var parsed_date = Date.parse(time_value);
-  var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
-  var delta = parseInt((relative_to.getTime() - parsed_date) / 1000);
-  delta = delta + (relative_to.getTimezoneOffset() * 60);
-
-  if (delta < 60) {
-    return 'less than a minute ago';
-  } else if(delta < 120) {
-    return 'about a minute ago';
-  } else if(delta < (60*60)) {
-    return (parseInt(delta / 60)).toString() + ' minutes ago';
-  } else if(delta < (120*60)) {
-    return 'about an hour ago';
-  } else if(delta < (24*60*60)) {
-    return 'about ' + (parseInt(delta / 3600)).toString() + ' hours ago';
-  } else if(delta < (48*60*60)) {
-    return '1 day ago';
-  } else {
-    return (parseInt(delta / 86400)).toString() + ' days ago';
-  }
+ 
+function formatTwitString(str) {
+	str=' '+str;
+	str = str.replace(/((ftp|https?):\/\/([-\w\.]+)+(:\d+)?(\/([\w/_\.]*(\?\S+)?)?)?)/gm,'<a href="$1" target="_blank">$1</a>');
+	str = str.replace(/([^\w])\@([\w\-]+)/gm,'$1<a href="http://twitter.com/$2" target="_blank">@$2</a>');
+	str = str.replace(/([^\w])\#([\w\-]+)/gm,'$1<a href="http://twitter.com/search?q=%23$2" target="_blank">#$2</a>');
+	return str;
+}
+ 
+function relativeTime(pastTime) {	
+	var origStamp = Date.parse(pastTime);
+	var curDate = new Date();
+	var currentStamp = curDate.getTime();	
+	var difference = parseInt((currentStamp - origStamp)/1000);
+	if(difference < 0) return false;
+	if(difference <= 5) return "Just now";
+	if(difference <= 20) return "Seconds ago";
+	if(difference <= 60) return "A minute ago";
+	if(difference < 3600) return parseInt(difference/60)+" minutes ago";
+	if(difference <= 1.5*3600) return "One hour ago";
+	if(difference < 23.5*3600) return Math.round(difference/3600)+" hours ago";
+	if(difference < 1.5*24*3600) return "One day ago";	
+	var dateArr = pastTime.split(' ');
+	return dateArr[4].replace(/\:\d+$/,'')+' '+dateArr[2]+' '+dateArr[1]+(dateArr[3]!=curDate.getFullYear()?' '+dateArr[3]:'');
 }
